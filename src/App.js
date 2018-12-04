0fed948fb8153a4b70313a0caf67db489b2aff21
Peter Kfoury <pkfoury@purdue.edu>
2018-12-04 17:22:44 -0500

    nav

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home'
 import Login from './pages/Login';
+import Nav from './components/GlobalNav';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Router>
-        <div>
-        <Route exact path='/' component={Home} />
-        <Route path='/login' component={Login} />
-        </div>
-      </Router>
+        <Nav />
+        <Router>
+          <div>
+            <Route exact path='/' component={Home} />
+            <Route path='/login' component={Login} />
+          </div>
+        </Router>
       </div>
     );
   }
