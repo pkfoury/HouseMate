746526143a204fd2389787c142e91e08985b6cce
Peter Kfoury <pkfoury@purdue.edu>
2018-12-05 12:53:29 -0500

    adds non-authed homes endpoint

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ require('dotenv').config()
 const app = express();
 
 const db = mysql.createConnection({
-	host: '127.0.0.1',
-	user: process.env.MYSQL_USER,
+  host: '127.0.0.1',
+  user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: 'housemate'
 });
@@ -16,17 +16,21 @@ db.connect();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// get homes
+app.get('/homes', (req, res) => {
+  db.query('SELECT * FROM homes', function (error, results) {
+    if (error) throw error;
+    res.send(results);
+  });
+})
 
-// app.get('/', (req, res) => {
-// 	db.query('SELECT * FROM homes', function (error, results) {
-// 		if (error) throw error;
-// 		res.send(results);
-// 	});
-// })
+// serve react site
+if (process.env.NODE_ENV == 'prod') {
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
 
 app.listen(8080, () => {
-	console.log("listening on 8080");
+  console.log("listening on 8080");
 });
