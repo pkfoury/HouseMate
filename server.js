a0ad09bede784bc704cda9f4fb2b58d34af28ca4
Peter Kfoury <pkfoury@purdue.edu>
2018-12-05 03:12:34 -0500

    sets up static serving

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql')
+const path = require('path');
 require('dotenv').config()
 
 const app = express();
@@ -13,12 +14,18 @@ const db = mysql.createConnection({
 
 db.connect();
 
-app.get('/', (req, res) => {
-	db.query('SELECT * FROM homes', function (error, results) {
-		if (error) throw error;
-		res.send(results);
-	});
-})
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// app.get('/', (req, res) => {
+// 	db.query('SELECT * FROM homes', function (error, results) {
+// 		if (error) throw error;
+// 		res.send(results);
+// 	});
+// })
 
 app.listen(8080, () => {
 	console.log("listening on 8080");
