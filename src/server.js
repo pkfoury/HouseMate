f84d2d97db33876b1e74634d2cd031f817454d4b
Peter Kfoury <pkfoury@purdue.edu>
2018-12-05 02:08:20 -0500

    set up mysql

--- src/server.js
+++ src/server.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const mysql = require('mysql')
+require('dotenv').config()
 
 const app = express();
 
+const db = mysql.createConnection({
+	host: '127.0.0.1',
+	user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: 'housemate'
+});
+
+db.connect();
+
 app.get('/', (req, res) => {
-	res.send('hey');
+	db.query('SELECT * FROM homes', function (error, results) {
+		if (error) throw error;
+		res.send(results);
+	});
 })
 
 app.listen(8080, () => {
