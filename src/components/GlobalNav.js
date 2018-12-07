514e0bc1e4ed3fbc849c06d92e9429aad78b25ca
julzsd12 <julzsd12@gmail.com>
2018-12-07 00:31:12 -0500

    added register to nav bar

--- src/components/GlobalNav.js
+++ src/components/GlobalNav.js
@@ -35,6 +35,10 @@ export default class GlobalNav extends Component {
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
+            <NavItem>
+                <NavLink href="/register">Register</NavLink>
+              </NavItem>
+
               <NavItem>
                 <NavLink href="/login">Login</NavLink>
               </NavItem>
@@ -44,23 +48,7 @@ export default class GlobalNav extends Component {
               <NavItem>
                 <NavLink href="/tasks">Tasks</NavLink>
               </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
+              
             </Nav>
           </Collapse>
         </Navbar>
